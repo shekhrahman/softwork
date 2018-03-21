54d56ecbefdbea10b35717a781f28c5df7ad94a8
Rahman Shaikh <shekhraheman@gmail.com>
2018-03-21 20:19:52 +0530

    Add files via upload
    Login page code in html

---
+++ log.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="css/log.css">
+
+<div class="log">
+    <h2>login form</h2>
+    Username:<input type="text" name="user" placeholder="Username" required><br>
+    
+    Password:<input type="text" name="pass" placeholder="password" required class="pass">
+    
+    <input type="submit" name="sub" value="Submit >>">
+</div>
